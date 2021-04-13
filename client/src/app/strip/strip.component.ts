2bf2121d573476172e0f4d563460493f309dcacf
ruslanNir <nirkuba19999@gmail.com>
2021-04-13 10:31:52 +0300

    dispaly movie at app-strip comp

--- client/src/app/strip/strip.component.ts
+++ client/src/app/strip/strip.component.ts
@@ -1,25 +1,15 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Movie } from '../models/Movie';
 
 
 @Component({
-	selector: 'app-strip',
-	templateUrl: './strip.component.html'
+   selector: 'app-strip',
+   templateUrl: './strip.component.html'
 })
 
 export class StripComponent implements OnInit {
+   @Input() movie: Movie;
 
-	// interface movie{
-	// 	id:number;
-	// 	name:string;
-	// };
-	
-	// @Input()aaa:movie;
-
-
-
-
-
-	
-	ngOnInit(): void {
-	}
+   ngOnInit(): void {
+   }
 }
